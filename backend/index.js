a28d51b4f926e9cb2a304186c3298642d5924c17
Mahmood Alam <mahmoodalam2601@gmail.com>
2026-02-08 18:44:19 +0530

    url changes

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,8 @@ app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
 const corsOptions={
-    origin:'http://localhost:5173',
+    // origin:'http://localhost:5173',
+    origin:'https://job-portalapp-kbcz.onrender.com',
     credentials:true
 }
 
